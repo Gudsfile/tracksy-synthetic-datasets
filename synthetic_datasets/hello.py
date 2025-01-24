3a86f5de5b2334bbbfaa838806d3bcde19c7d806
Gudsfile <gudsfile@protonmail.com>
2025-01-24 22:02:49 +0100

    ✨  generate zip datasets

--- synthetic_datasets/hello.py
+++ synthetic_datasets/hello.py
@@ -4,12 +4,12 @@ from pathlib import Path
 
 from .factories.factory_config import FactoryConfig
 from .factories.spotify import generate_streamings, init_faker
-from .writers.spotify import DEFAULT_EXTENSION, DEFAULT_FOLDER, DEFAULT_PREFIX, write
+from .writers.spotify import DEFAULT_EXTENSION, DEFAULT_FOLDER, DEFAULT_PREFIX, write, write_zip
 
 
 def spotify(nb_streams):
-    write(
-        generate_streamings(
+    def generator(nb_streams):
+        yield generate_streamings(
             init_faker(),
             FactoryConfig(
                 nb_streams=nb_streams,
@@ -18,8 +18,19 @@ def spotify(nb_streams):
                 nb_albums=int(nb_streams * 0.3),
                 nb_artists=int(nb_streams * 0.2),
             ),
-        ),
+        )
+
+    write(
+        next(generator(nb_streams)),
         Path(f"datasets/{DEFAULT_FOLDER}/{DEFAULT_PREFIX}_2024-2025_{nb_streams}{DEFAULT_EXTENSION}"),
+    )
+    chunk_size = 20000
+    minimum_nb_file = 4
+    nb_file_to_write = max(int(nb_streams / chunk_size), minimum_nb_file)
+
+    write_zip(
+        [next(generator(int(nb_streams / nb_file_to_write))) for _ in range(0, nb_file_to_write)],
+        Path(f"datasets/{DEFAULT_FOLDER}/"),
     )
 
 
@@ -31,10 +42,9 @@ def main():
     if not args[0].isdigit():
         print(f"Number of lines to be generated should be integer. Given arg: {args[0]}")
         sys.exit(1)
-
-    start_pydantic = time.time()
+    start_data_generation = time.time()
     spotify(int(args[0]))
-    print("--- %s seconds ---" % (time.time() - start_pydantic))
+    print("--- %s seconds ---" % (time.time() - start_data_generation))
 
 
 if __name__ == "__main__":
